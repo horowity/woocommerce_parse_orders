2821644b667c3d6b539c121ae984943e4e903b76
Yehezkel Horowitz <horowity@gmail.com>
2024-02-13 00:18:55 +0200

    Start date + Summary file

--- parse_orders.py
+++ parse_orders.py
@@ -1,22 +1,30 @@
 import argparse
 import csv
 from config import modify_list
+import xlsxwriter
 
 orders = []
 ORDER_ITEMS = 'מוצרים'
 ITEMS_SOLD  = 'Items sold'
-NET_REVENUE = 'N. הכנסה'
+#NET_REVENUE = 'N. הכנסה'
+NET_REVENUE = "Net sales"
+DATE = 'תאריך'
+SOLIDERS = 'הנחת חיילים'
+REGULAR = 'הזמנה רגילה'
 
 product_names = []
 
 def parse_order_items(row):
     try:
+        #print(row)
         row[NET_REVENUE] = row[NET_REVENUE].replace("'", "")
         row[ITEMS_SOLD] = row[ITEMS_SOLD].replace("'", "")
+
         orig_items = row[ORDER_ITEMS]           
         items = orig_items
         row.pop(ORDER_ITEMS)
         if not orig_items:
+            print(f"No items in {row}")
             return
         #print(items)
         # Loop over modify list to change item names with comma into something else 
@@ -34,22 +42,69 @@ def parse_order_items(row):
             if name not in product_names:
                 product_names.append(name)
             #print(f"name {name} amount {amount}")
-            row[name] = amount
+            if name == SOLIDERS:
+                amount = 1
+            row[name] = int(amount)
     except Exception as e:
-        print(orig_items)
+        print(row)
         print(e)
 
 
-def parse_orders(input_file_name, output_file_name):
+def generate_summary(summary_file_name, orders):
+    orders_summary = {}
+    summary_products = []
+    for product in product_names:
+        if product != SOLIDERS:
+            summary_products.append(product)
+            orders_summary[product] = { REGULAR: 0, SOLIDERS: 0}
+
+    for order in orders:
+        key = REGULAR
+        if SOLIDERS in order.keys():
+            key = SOLIDERS
+        for product in summary_products:
+            if product in order.keys():
+                orders_summary[product][key] += order[product]
+
+    # Create a workbook and add a worksheet.
+    workbook = xlsxwriter.Workbook(summary_file_name)
+    worksheet = workbook.add_worksheet()
+    # Start from the first cell. Rows and columns are zero indexed.
+    row = 0
+    col = 0
+
+    # Write the header
+    worksheet.write(row, col,     ORDER_ITEMS)
+    worksheet.write(row, col + 1, REGULAR)
+    worksheet.write(row, col + 2, SOLIDERS)
+    row += 1
+    # Iterate over the data and write it out row by row.
+    for product in summary_products:
+        worksheet.write(row, col,     product)
+        worksheet.write(row, col + 1, orders_summary[product][REGULAR])
+        worksheet.write(row, col + 2, orders_summary[product][SOLIDERS])
+        row += 1
+
+    workbook.close()
+
+def parse_orders(input_file_name, output_file_name, summary_file_name, start_date):
 
     with open(input_file_name, 'r') as file:
         reader = csv.DictReader(file)
 
         for row in reader:
-            #this will also populate the global product_names
-            parse_order_items(row)
-            orders.append(row)
-            
+            # DATE is the first cell, but as there is a strange unicode issue with it,
+            # just take the first key with this simple loop over the row keys
+            # date = row[DATE]
+            for k in row.keys():
+                d = k
+                break
+            date = row[d]
+            if start_date and start_date < date:
+                #this will also populate the global product_names
+                parse_order_items(row)
+                orders.append(row)
+
         out_columns = reader.fieldnames
         out_columns.remove(ORDER_ITEMS)
         product_names.sort()
@@ -62,13 +117,16 @@ def parse_orders(input_file_name, output_file_name):
         writer.writeheader()
         writer.writerows(orders)
 
+    generate_summary(summary_file_name, orders)
 
 def main():
     parser = argparse.ArgumentParser(description='Parse Woocommerce orders')
     parser.add_argument('input_file', type=str, help='Input file - CSV you got from Woocommerce')
     parser.add_argument('output_file', type=str, help='Output file - name of output file to create')
+    parser.add_argument('summary_file', type=str, help='Output file - name of output file to create')
+    parser.add_argument('--start_date', type=str, default="2023-01-01 00:00:00", help='Start processing from this date')
     args = parser.parse_args()
-    parse_orders(args.input_file, args.output_file)
+    parse_orders(args.input_file, args.output_file, args.summary_file, args.start_date)
     
 if __name__ == '__main__':
     main()
